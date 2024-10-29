f5d5de988342b57454d0b916179d680cb195ee48
seanquiambao <slquiambao03@gmail.com>
2024-10-28 17:01:52 -0700

    Fix

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -2,7 +2,7 @@ interface props {
   name: string;
 }
 
-const Item = ({ name, style }: props) => {
+const Item = ({ name }: props) => {
   return <div className="bg-blue-800 p-5 text-4xl text-white">{name}</div>;
 };
 
